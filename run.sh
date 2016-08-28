119e0ab5d6505b1ebf64569505a0e2373fda8fa1
vik_y <vikas.yadav@iiitb.org>
2016-08-28 17:17:28 +0530

    Added incomplete single script to collect data

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# A single script to download and run the program on client side. 
+# Remember that on client side it should be only one command and script 
+# should take care of all the dependencies etc. 
+
+
+# Install all dependencies here. 
+sudo pip install requests
+sudo pip install daemonize 
+
+# Switch to a new directory 
+mkdir network-stat
+cd network-stat 
+
+# Fetch the required files from github repo
+
+# Do whatever book keeping that is required. 
+
+# Run the program 
+
+
+
+
